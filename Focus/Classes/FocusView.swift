4bb923788f0239dba850dc1f9c5d5403db6d5caa
Isaac Charny <isaac.charny@gmail.com>
2016-10-19 10:50:55 -0400

    Migrated to Swift 3

--- Focus/Classes/FocusView.swift
+++ Focus/Classes/FocusView.swift
@@ -9,30 +9,30 @@
 import Foundation
 import UIKit
 
-public class FocusView: UIView {
+open class FocusView: UIView {
     
     public enum FocusType {
-        case Square
-        case Circle
+        case square
+        case circle
     }
     
     public enum FocusStyle {
-        case Fade
-        case Blur
+        case fade
+        case blur
     }
     
-    public var type: FocusType = .Circle
-    public var style: FocusStyle = .Blur
-    private var fillLayer: CAShapeLayer?
-    private var active: Bool!
+    open var type: FocusType = .circle
+    open var style: FocusStyle = .blur
+    fileprivate var fillLayer: CAShapeLayer?
+    fileprivate var active: Bool!
     
     // Translucency Properties
-    public var opacity: Float = 0.5
-    public var color: UIColor = UIColor.blackColor()
+    open var opacity: Float = 0.5
+    open var color: UIColor = UIColor.black
     
     // Blur Properties
-    public var blurStyle: UIBlurEffectStyle = .Dark
-    private var blur: UIVisualEffectView?
+    open var blurStyle: UIBlurEffectStyle = .dark
+    fileprivate var blur: UIVisualEffectView?
     
     
     required public init?(coder aDecoder: NSCoder) {
@@ -47,38 +47,38 @@ public class FocusView: UIView {
     
     func setup() {
         active = false
-        self.userInteractionEnabled = false
+        self.isUserInteractionEnabled = false
     }
     
-    public func focusOnView(theView:UIView,
-                            focusType type:FocusType = FocusType.Circle,
-                            focusStyle style:FocusStyle = FocusStyle.Fade,
+    open func focusOnView(_ theView:UIView,
+                            focusType type:FocusType = FocusType.circle,
+                            focusStyle style:FocusStyle = FocusStyle.fade,
                             focusPadding padding:CGFloat = 0.0) {
         self.type = type
         self.style = style
-        let rect = theView.superview!.convertRect(theView.frame, toView: self)
+        let rect = theView.superview!.convert(theView.frame, to: self)
         let path = UIBezierPath(rect: CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height))
         var focusPath: UIBezierPath?
         switch type {
-        case .Circle:
+        case .circle:
             let diagonal = sqrt(pow(rect.width, 2) + pow(rect.height, 2))
             let radius = diagonal / 2 + padding
             focusPath = UIBezierPath(roundedRect: CGRect(x: rect.midX - radius, y: rect.midY - radius, width: 2*radius, height: 2*radius), cornerRadius: CGFloat(radius))
-        case .Square:
+        case .square:
             focusPath = UIBezierPath(rect: CGRect(x: rect.origin.x, y: rect.origin.y, width: rect.width, height: rect.height))
         }
         if let focusPath = focusPath {
-            path.appendPath(focusPath)
+            path.append(focusPath)
             path.usesEvenOddFillRule = true
             fillLayer = CAShapeLayer()
-            fillLayer!.path = path.CGPath
+            fillLayer!.path = path.cgPath
             fillLayer!.fillRule = kCAFillRuleEvenOdd
             switch style {
-            case .Fade:
-                fillLayer!.fillColor = color.CGColor
+            case .fade:
+                fillLayer!.fillColor = color.cgColor
                 fillLayer!.opacity = opacity
                 self.layer.addSublayer(fillLayer!)
-            case .Blur:
+            case .blur:
                 blur = UIVisualEffectView(effect: UIBlurEffect(style: blurStyle))
                 blur!.frame = self.frame
                 self.addSubview(blur!)
@@ -89,9 +89,9 @@ public class FocusView: UIView {
         
     }
     
-    public func removeFocus() {
+    open func removeFocus() {
         if isActive() {
-            if style == .Blur {
+            if style == .blur {
                 blur!.removeFromSuperview()
                 blur = nil
             }
@@ -101,7 +101,7 @@ public class FocusView: UIView {
         }
     }
     
-    public func isActive() -> Bool {
+    open func isActive() -> Bool {
         return self.active
     }
     
